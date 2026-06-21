d70119141b5d84685d1dbe87e26cc92ee4aa0806
Babaiar <119310971+Babaiaro@users.noreply.github.com>
2026-06-20 21:28:31 -0500

    Add files via upload

---
+++ tester.java
@@ -0,0 +1,26 @@
+package jun.error;
+
+public class tester {
+    public static void main(String[] args) {
+        try {
+            test();
+        } catch ( ApplicationException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    public static void test() throws ApplicationException{
+        try {
+            int a = 0;
+            int b = 1; 
+            System.out.println(b/a);
+        } catch (Exception e) {
+            throw new ApplicationException("Chained Exception", e);
+        }
+    }     
+}
+ class ApplicationException extends Exception{
+        public ApplicationException(String message, Exception cause){
+            super(message, cause);
+        }
+}
